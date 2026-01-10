213dc353942173e90bbd33f1940760eca9a6919f
abhaya6996 <140595674+abhaya6996@users.noreply.github.com>
2026-01-10 11:26:22 +0545

    Add files via upload

---
+++ songlyrics/includes/config.php
@@ -0,0 +1,32 @@
+<?php
+// Database config
+
+$dbHost = 'localhost';
+$dbUser = 'root';
+$dbPass = '';
+$dbName = 'lyricsdb';
+
+// Start session
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Connect to DB
+$conn = new mysqli($dbHost, $dbUser, $dbPass, $dbName,3307);
+
+if ($conn->connect_error) {
+    die('Database connection failed: ' . $conn->connect_error);
+}
+$conn->set_charset('utf8mb4');
+
+// Base URL of your project (folder name after localhost)
+define("BASE_URL", "/songlyrics/");
+
+
+// Helper functions
+function isLoggedIn(): bool {
+    return !empty($_SESSION['user']);
+}
+function isAdmin(): bool {
+    return isLoggedIn() && $_SESSION['user']['role'] === 'admin';
+}
